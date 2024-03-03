08cb219d0c77713c59e1379cc0c47c4e8c5a2c33
BelL <tinnakornchoompee@gmail.com>
2024-03-03 17:05:44 +0700

    Add Color

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Text('Hello world!')));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 26, 2, 80),
+              Color.fromARGB(255, 45, 7, 98)
+            ]),
+          ),
+          child: const Center(
+            child: Text('Hello World'),
+          ),
+        ),
+      ),
+    ),
+  );
 }
